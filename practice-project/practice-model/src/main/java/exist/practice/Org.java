d14fbecb2a672825c8fade85c9cd77296fe161c5
Romel Campos <exst_rcampos@yahoo.com>
2011-07-13 18:52:59 +0800

    formatted models

--- practice-project/practice-model/src/main/java/exist/practice/Org.java
+++ practice-project/practice-model/src/main/java/exist/practice/Org.java
@@ -6,45 +6,49 @@ import exist.practice.User;
 
 @Entity
 public class Org {
-    
+
 	@Id
 	@GeneratedValue
-    private long orgId = -1;
-    private String orgName;
-    @ManyToMany(cascade = CascadeType.ALL, fetch=FetchType.EAGER)
-    @JoinTable( name = "OrgMembers", joinColumns = @JoinColumn( name = "orgId" ),
-            inverseJoinColumns = @JoinColumn( name = "userId" ) )
-    private Set<User> members;
+	private long orgId = -1;
+	private String orgName;
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinTable(name = "OrgMembers", joinColumns = @JoinColumn(name = "orgId"), inverseJoinColumns = @JoinColumn(name = "userId"))
+	private Set<User> members;
 
 	/**
-     * @return the orgId
-     */
-    public long getOrgId() {
-        return orgId;
-    }
-    /**
-     * @param orgId the orgId to set
-     */
-    public void setOrgId(long orgId) {
-        this.orgId = orgId;
-    }
-    /**
-     * @return the orgName
-     */
-    public String getOrgName() {
-        return orgName;
-    }
-    /**
-     * @param orgName the orgName to set
-     */
-    public void setOrgName(String orgName) {
-        this.orgName = orgName;
-    }
-    
+	 * @return the orgId
+	 */
+	public long getOrgId() {
+		return orgId;
+	}
+
+	/**
+	 * @param orgId
+	 *            the orgId to set
+	 */
+	public void setOrgId(long orgId) {
+		this.orgId = orgId;
+	}
+
+	/**
+	 * @return the orgName
+	 */
+	public String getOrgName() {
+		return orgName;
+	}
+
+	/**
+	 * @param orgName
+	 *            the orgName to set
+	 */
+	public void setOrgName(String orgName) {
+		this.orgName = orgName;
+	}
+
 	public Set<User> getMembers() {
 		return members;
 	}
-    
+
 	public void setMembers(Set<User> members) {
 		this.members = members;
 	}
