3683f6c508db8ab8c3b0133f2b9d1dbe7ca5ce88
Mark Diaz Ng <markng_08@live.com>
2011-07-13 16:44:22 +0800

    added models

---
+++ practice-project/practice-model/src/main/java/exist/practice/User.java
@@ -0,0 +1,168 @@
+package exist.practice;
+
+import java.util.*;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    
+	@Id
+    private long userId = -1;
+    private String userName;
+    private String password;
+    private String lastName;
+    private String firstName;
+    private char mi;
+    private String emailAddress;
+    private String gender;
+    private Date birthDate;
+    private String homeAddress;
+    private String contactNumber;
+    @ManyToMany(cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+    @JoinTable( name = "UserOrgs", joinColumns = @JoinColumn( name = "userId" ),
+            inverseJoinColumns = @JoinColumn( name = "orgId" ) )
+    private Set<Org> orgs;
+    
+    /**
+     * @return the userId
+     */
+    public long getUserId() {
+        return userId;
+    }
+    /**
+     * @param userId the userId to set
+     */
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    /**
+     * @return the mi
+     */
+    public char getMi() {
+        return mi;
+    }
+    /**
+     * @param mi the mi to set
+     */
+    public void setMi(char mi) {
+        this.mi = mi;
+    }
+    /**
+     * @return the emailAddress
+     */
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+    /**
+     * @param emailAddress the emailAddress to set
+     */
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    /**
+     * @return the birthDate
+     */
+    public Date getBirthDate() {
+        return birthDate;
+    }
+    /**
+     * @param birthDate the birthDate to set
+     */
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+    /**
+     * @return the homeAddress
+     */
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+    /**
+     * @param homeAddress the homeAddress to set
+     */
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+    /**
+     * @return the contactNumber
+     */
+    public String getContactNumber() {
+        return contactNumber;
+    }
+    /**
+     * @param contactNumber the contactNumber to set
+     */
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+    
+	public void setOrgs(Set<Org> orgs) {
+		this.orgs = orgs;
+	}
+	
+	public Set<Org> getOrgs() {
+		return orgs;
+	}
+    
+}
