6fda422b01cdcfd1f226b56ca16959d767d5667b
Mark Diaz Ng <markng_08@live.com>
2011-07-14 19:14:54 +0800

    added testcase for dao

--- practice-project/practice-dao/src/test/java/exist/practice/dao/impl/GenericDaoImplTest.java
+++ practice-project/practice-dao/src/test/java/exist/practice/dao/impl/GenericDaoImplTest.java
@@ -2,35 +2,41 @@ package exist.practice.dao.impl;
 
 import static org.junit.Assert.assertTrue;
 
-import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import exist.practice.User;
 import exist.practice.dao.GenericDao;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "/practiceProject-test-config.xml" })
 public class GenericDaoImplTest {
 
-	
-	private GenericDao<User> genericDAO;
-	private SessionFactory sessionFactory;
+	private GenericDao<User> genericDao;
 
-
-	public void setGenericDAO(GenericDao<User> genericDAO) {
-		this.genericDAO = genericDAO;
-	}
-
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
+	@Autowired
+	public void setGenericDAO(GenericDao<User> genericDao) {
+		this.genericDao = genericDao;
 	}
 
 	@Test
-	public void TestAdd() {
+	public void testAdd() {
 		User user = new User();
 		user.setUserName("test-username-1");
-		//assertTrue("User 1 added success", genericDAO.add(user));
-		assertTrue(true);
+		assertTrue("User 1 added success", genericDao.add(user));
+		//assertTrue(true);
+	}
+	
+	@Test
+	public void testDelete(){
+		User user = new User();
+		user.setUserName("test-username-1");
+		assertTrue("User 1 added:", genericDao.add(user));
+		assertTrue("Delete User 1:",genericDao.delete(1, User.class));
+		
 	}
 
 }
