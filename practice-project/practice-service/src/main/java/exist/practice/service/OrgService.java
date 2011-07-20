cb44187b9639a335cd53c1ca28adfb43c459ada0
Glenn Guden <glenn_guden@yahoo.com>
2011-07-20 17:21:15 +0800

    edited OrgServiceImpl and OrgService

--- practice-project/practice-service/src/main/java/exist/practice/service/OrgService.java
+++ practice-project/practice-service/src/main/java/exist/practice/service/OrgService.java
@@ -12,8 +12,8 @@ public interface OrgService {
 	
 	public boolean updateOrg(Org object);
 
-	public boolean deleteOrg(long id, Class<Org> clazz);
+	public boolean deleteOrg(long id);
 
-	public List<Org> findOrg(String table, String column, String value);
+	public List<Org> findOrg(String column, String value);
 
 }
