a0ad4eb96879236596c830274470264912c2d4a4
Romel Campos <exst_rcampos@yahoo.com>
2011-07-20 18:23:11 +0800

    asd

--- practice-project/practice-webapp/src/main/java/exist/practice/controller/MainMultiController.java
+++ practice-project/practice-webapp/src/main/java/exist/practice/controller/MainMultiController.java
@@ -2,6 +2,7 @@ package exist.practice.controller;
 
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
