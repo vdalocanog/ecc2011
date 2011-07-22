a8db63fb27c8f5b96f57cefd3c6a4f7de7bfbdf0
Glenn Guden <glenn_guden@yahoo.com>
2011-07-22 11:04:46 +0800

    added editUser

--- practice-project/practice-webapp/src/main/java/exist/practice/controller/WebAppMiniProjMultiActionController.java
+++ practice-project/practice-webapp/src/main/java/exist/practice/controller/WebAppMiniProjMultiActionController.java
@@ -1,7 +1,9 @@
 package exist.practice.controller;
 
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import exist.practice.Org;
 import exist.practice.User;
+import exist.practice.service.OrgService;
 import exist.practice.service.UserService;
 
 
@@ -23,24 +26,35 @@ public class WebAppMiniProjMultiActionController {
 	
 	private UserService userService;
 	
+	private OrgService orgService;
+	
 	@Autowired
-	public void setUserServiceImpl(UserService userService) {
+	public void setUserService(UserService userService) {
 		this.userService = userService;
 	}
+	
+	@Autowired
+    public void setOrgService(OrgService orgService) {
+        this.orgService = orgService;
+    }
 
     @RequestMapping("/viewProfile.htm")
     public ModelAndView viewProfile(HttpServletRequest req, HttpServletResponse res) throws Exception {
         ModelAndView mav = new ModelAndView("viewProfile");
         
         User user = new User();
+        
+        /*
         user.setFirstName( "Glenn" );
         Set<Org> orgList = new HashSet<Org>();
         Org org1 = new Org(); org1.setOrgId( 1 ); org1.setOrgName( "A" ); orgList.add( org1 );
         Org org2 = new Org(); org2.setOrgId( 2 ); org2.setOrgName( "B" ); orgList.add( org2 );
         user.setOrgs( orgList );
+        */
+        
+        user = userService.findUser("userId", "2").get(0);
+        
         mav.addObject( "user", user );
-        
-        
         mav.addObject( "orgList", user.getOrgs() );
         
         return mav;
@@ -50,10 +64,9 @@ public class WebAppMiniProjMultiActionController {
     public ModelAndView viewOrgs(HttpServletRequest req, HttpServletResponse res) throws Exception {
         ModelAndView mav = new ModelAndView("viewOrgs");
  
-        Set<Org> orgList = new HashSet<Org>();
-        Org org1 = new Org(); org1.setOrgId( 1 ); org1.setOrgName( "A" ); orgList.add( org1 );
-        Org org2 = new Org(); org2.setOrgId( 2 ); org2.setOrgName( "B" ); orgList.add( org2 );
- 
+        List<Org> orgList = new ArrayList<Org>();
+        orgList = orgService.findAllOrg();
+        
         mav.addObject( "orgList", orgList );
         
         return mav;
