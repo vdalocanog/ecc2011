061dc9f8e413858245a377d803a7f92998ff7c08
Glenn Guden <glenn_guden@yahoo.com>
2011-08-05 10:43:33 +0800

    added log-in selenium testing

---
+++ practice-project-selenium-tests/src/test/java/exist/practice/AdminLoginTest.java
@@ -0,0 +1,48 @@
+package exist.practice;
+
+import com.thoughtworks.selenium.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.regex.Pattern;
+
+import junit.framework.Assert;
+
+public class AdminLoginTest extends SeleneseTestCase {
+	@Before
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://localhost:8080/");
+		selenium.start();
+	}
+
+	@Test
+	public void testAdminLogin() throws Exception {
+	    selenium.setSpeed("1000");
+	    
+		selenium.open("/practice-webapp/auth/login.htm?error=true");
+		selenium.type("id=username", "gg");
+		selenium.click("css=input[type=submit]");
+		selenium.waitForPageToLoad("30000");
+		//assertTrue(selenium.isTextPresent("Invalid Credentials"));
+		assertTrue(selenium.getText("css=#error").equals("Invalid Credentials"));
+		
+		selenium.open("/practice-webapp/auth/login.htm?error=true");
+		selenium.type("id=username", "admin");
+		selenium.type("id=password", "qwer");
+		selenium.click("css=input[type=submit]");
+		selenium.waitForPageToLoad("30000");
+		assertTrue(selenium.getText("css=#error").equals("Invalid Credentials"));
+		
+		selenium.open("/practice-webapp/auth/login.htm?error=true");
+		selenium.type("id=username", "admin");
+		selenium.type("id=password", "admin");
+		selenium.click("css=input[type=submit]");
+		selenium.waitForPageToLoad("30000");
+		assertTrue(selenium.getText("css=h1").equals("Welcome"));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+}
